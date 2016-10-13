b5e7e59099f078d886286e05fe6931ab6ca8c37b
William Reilly <wreilly2001@gmail.com>
2016-10-13 18:18:53 -0400

    commit midway working on Weather API call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,3 @@
 const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
 
-console.log("WR__ actions/index.js OPENWEATHER_API_KEY: ", OPENWEATHER_API_KEY);
+console.log("WR__ 987 actions/index.js OPENWEATHER_API_KEY: ", OPENWEATHER_API_KEY);
